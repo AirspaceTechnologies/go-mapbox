82934f094fa87da9d5f6141d98b827aad4b1148d
oliverpool <git@olivier.pfad.fr>
2023-04-18 09:00:50 +0200

    move mockClient to client_test.go

--- geocoding_test.go
+++ geocoding_test.go
@@ -2,38 +2,9 @@ package mapbox
 
 import (
 	"context"
-	"errors"
 	"net/http"
 	"testing"
 )
-
-type roundTripperFunc func(*http.Request) (*http.Response, error)
-
-func (rt roundTripperFunc) RoundTrip(req *http.Request) (*http.Response, error) {
-	return rt(req)
-}
-
-func mockClient(responses ...*http.Response) (*Client, chan *http.Request) {
-	ch := make(chan *http.Request)
-	i := 0
-	client := &Client{
-		httpClient: &http.Client{
-			Transport: roundTripperFunc(func(r *http.Request) (*http.Response, error) {
-				ch <- r
-				if i > len(responses) {
-					return nil, errors.New("mockClient: not enough responses")
-				}
-				resp := responses[i]
-				i++
-				if i > len(responses) {
-					close(ch)
-				}
-				return resp, nil
-			}),
-		},
-	}
-	return client, ch
-}
 
 func checkforwardGeocodeRequestURL(t *testing.T, req *ForwardGeocodeRequest, expectedURL string) {
 	t.Helper()
